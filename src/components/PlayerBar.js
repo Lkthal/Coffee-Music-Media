823bba7ea73be8ce9b4604d77ddda3b8c50c4f83
Thal Syhabout <tsyhabout@gmail.com>
2018-01-27 04:39:47 -0800

    complete button assignment of react

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
-class PlayerBar extends Componet {
+class PlayerBar extends Component {
   render() {
     return (
-      <sectipn className="player-bar">
-        player bar goes here
+      <section className="player-bar">
+        <section id="buttons">
+       <button id="previous" onClick={this.props.handlePrevClick}>
+         <span className="ion-skip-backward"></span>
+       </button>
+       <button id="play-pause">
+         <span className={this.props.isPlaying ? 'ion-pause' :'ion-play'}></span>
+       </button>
+       <button id="next" onClick={this.props.handleNextClick}>
+         <span className="ion-skip-forward"></span>
+       </button>
+       </section>
+       <section id="time-control">
+         <div className="current-time">–:––</div>
+         <input type="range" className="seek-bar" value="0" />
+         <div className="total-time">–:––</div>
+       </section>
+       <section id="volume-control">
+         <div className="icon ion-volume-low"></div>
+         <input type="range" className="seek-bar" value="80" />
+         <div className="icon ion-volume-high"></div>
+       </section>
       </section>
     );
   }

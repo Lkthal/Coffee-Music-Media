72f484d3c52fcfce78d7b9e504944877c1097fa0
Thal Syhabout <tsyhabout@gmail.com>
2018-01-27 02:30:55 -0800

    add player bar

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlayerBar extends Componet {
+  render() {
+    return (
+      <sectipn className="player-bar">
+        player bar goes here
+      </section>
+    );
+  }
+}
+
+export default PlayerBar;

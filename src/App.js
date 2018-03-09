33363ad560e8be20b1be3b16afff4d5b5031cc9c
Thal Syhabout <tsyhabout@gmail.com>
2018-03-08 16:16:53 -0800

    fixed issue with missing file

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Library from './components/Library';
 import Album from './components/Album';
 import blocjamslogo from './images/blocjamslogo.png';
 
-
-
 class App extends Component {
   render() {
     return (
